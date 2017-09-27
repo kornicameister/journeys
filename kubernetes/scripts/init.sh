64e7cbe42294fe61c84e3b90173b7c3b53e2c221
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2017-09-27 08:57:06 +0200

    fix dashboard and helm init

--- kubernetes/scripts/init.sh
+++ kubernetes/scripts/init.sh
@@ -38,10 +38,16 @@ install() {
 }
 
 install_dashboard() {
-    kubectl apply -f https://git.io/kube-dashboard
+    kubectl apply -f \
+        https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
 }
 
 init_helm () {
+    kubectl create serviceaccount --namespace kube-system tiller
+    kubectl create clusterrolebinding tiller-cluster-rule \
+        --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+    kubectl patch deploy --namespace kube-system tiller-deploy \
+        -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
     helm init --upgrade --service-account=kubernetes-dashboard
 }
 
